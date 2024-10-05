90ecd2f795ae054e58bfa89ad741065c5cd40c62
Map987 <99967856+Map987@users.noreply.github.com>
2024-10-06 06:00:19 +0800

    Update video_uploader.py

--- bilibili_api/video_uploader.py
+++ bilibili_api/video_uploader.py
@@ -31,7 +31,64 @@ from .exceptions.ResponseCodeException import ResponseCodeException
 
 _API = get_api("video_uploader")
 
+####
+from functools import reduce
+from hashlib import md5
+import urllib.parse
+import time
+import requests
 
+mixinKeyEncTab = [
+    46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
+    33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
+    61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+    36, 20, 34, 44, 52
+]
+
+def getMixinKey(orig: str):
+    '对 imgKey 和 subKey 进行字符顺序打乱编码'
+    return reduce(lambda s, i: s + orig[i], mixinKeyEncTab, '')[:32]
+
+def encWbi(params: dict, img_key: str, sub_key: str):
+    '为请求参数进行 wbi 签名'
+    mixin_key = getMixinKey(img_key + sub_key)
+    curr_time = round(time.time())
+    params['wts'] = curr_time                                   # 添加 wts 字段
+    params = dict(sorted(params.items()))                       # 按照 key 重排参数
+    # 过滤 value 中的 "!'()*" 字符
+    params = {
+        k : ''.join(filter(lambda chr: chr not in "!'()*", str(v)))
+        for k, v
+        in params.items()
+    }
+    query = urllib.parse.urlencode(params)                      # 序列化参数
+    wbi_sign = md5((query + mixin_key).encode()).hexdigest()    # 计算 w_rid
+    params['w_rid'] = wbi_sign
+    return params
+
+def getWbiKeys() -> tuple[str, str]:
+    '获取最新的 img_key 和 sub_key'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Referer': 'https://www.bilibili.com/'
+    }
+    resp = requests.get('https://api.bilibili.com/x/web-interface/nav', headers=headers)
+    resp.raise_for_status()
+    json_content = resp.json()
+    img_url: str = json_content['data']['wbi_img']['img_url']
+    sub_url: str = json_content['data']['wbi_img']['sub_url']
+    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
+    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
+    return img_key, sub_key
+def update_params_with_wbi(params):
+   img_key, sub_key = getWbiKeys()
+
+   signed_params = encWbi(params=params, img_key=img_key, sub_key=sub_key)
+   query = urllib.parse.urlencode(signed_params)
+   return query
+
+
+###
 async def upload_cover(cover: Picture, credential: Credential) -> str:
     """
     上传封面
@@ -348,7 +405,7 @@ class VideoMeta:
     source: Optional[str] = None  # 可选，视频来源。
     recreate: Optional[bool] = False  # 可选，是否允许重新上传。
     no_reprint: Optional[bool] = False  # 可选，是否禁止转载。
-    no_disturbance: Optional[bool] = False,  # 可选，是否禁止转载。
+    no_disturbance: Optional[bool] = False,  # 可选，是否动态显示该视频。
     open_elec: Optional[bool] = False  # 可选，是否展示充电信息。
     up_selection_reply: Optional[bool] = False  # 可选，是否开启评论精选。
     up_close_danmu: Optional[bool] = False  # 可选，是否关闭弹幕。
@@ -377,7 +434,7 @@ class VideoMeta:
         source: Optional[str] = None,  # 可选，视频来源。
         recreate: Optional[bool] = False,  # 可选，是否允许重新上传。
         no_reprint: Optional[bool] = False,  # 可选，是否禁止转载。
-        no_disturbance: Optional[bool] = False,  # 可选，是否禁止转载。
+        no_disturbance: Optional[bool] = False,  # 可选，是否动态显示该视频。
         open_elec: Optional[bool] = False,  # 可选，是否展示充电信息。
         up_selection_reply: Optional[bool] = False,  # 可选，是否开启评论精选。
         up_close_danmu: Optional[bool] = False,  # 可选，是否关闭弹幕。
@@ -1277,6 +1334,7 @@ class VideoUploader(AsyncEvent):
 
         try:
             params = {"csrf": self.credential.bili_jct, "t": time.time() * 1000}
+            params = update_params_with_wbi(params)
             # headers = {"content-type": "application/json"}
             # 已有 json_body，似乎不需要单独设置 content-type
             resp = (
